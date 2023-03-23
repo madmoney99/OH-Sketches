31cbd548d53a9e9465a80a819e01310cb2fba643
madmoney99 <tonygoodale@gmail.com>
2023-03-22 19:59:08 -0700

    CB Boards

--- HID_LCB/HID_LCB.ino
+++ HID_LCB/HID_LCB.ino
@@ -1,45 +1,13 @@
 
-/*** @file SIM_PANEL.ino>
-/** @author <Tony Goodale>
- * @date <Mar20 23>
- * @brief <SIM PANEL DCS BIOS>
- *
- *Ideas
-Toggles
-- Rearm menu
-
-Rotary
-- Cockpit
-- LSO
-- Bomb
-- Missile
-- F10 Map
- * 
- */
-
-#include "Joystick.h"
-#define NUMBUTTONS 15
-#define hdtrk1 4 
-#define hdtrk2 3
-#define time 2 
-#define toggle1 A0 
-#define toggle2 A1
-#define game1 A2
-#define game2 A3
-#define rot1 15
-#define rot2 14
-#define rot3 16
-#define rot4 10
-#define rot5 9
-#define rot6 8
-#define rot7 7 
-#define rot8 6
+//HID Panel for LCB PANEL
+#include <Joystick.h>
+#define NUMBUTTONS 9     // match to number of pins being used for HID
 
 // defining the total [#] of buttons and their pins
-int SwitchOnPin[NUMBUTTONS] = {hdtrk1,hdtrk2,time,toggle1,toggle2,game1,game2,rot1,rot2,rot3,rot4,rot5,rot6,rot7,rot8};
+int SwitchOnPin[NUMBUTTONS] = {A0,2,3,4,5,6,7,8,9};    //add other pins here
 //Store States
-bool lastBtnState[NUMBUTTONS] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-bool btnState[NUMBUTTONS] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+bool lastBtnState[NUMBUTTONS] = {0};      // match number of input pins, 3 would show as 0,0,0
+bool btnState[NUMBUTTONS] = {0};      // same as above
 
 Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,JOYSTICK_TYPE_JOYSTICK,
   NUMBUTTONS, 0,                  // Button Count, Hat Switch Count
@@ -67,6 +35,5 @@ void loop() {
       lastBtnState[i] = btnState;
     }
   }
- 
-  delay(50);
+   delay(50);
 }
